e023ee6391e56db80ff6c08562657a0fbb013836
emags112 <magnussen.evan@gmail.com>
2019-06-01 10:17:24 -0400

    handling edge cases

--- graphql/helpers.js
+++ graphql/helpers.js
@@ -3,7 +3,8 @@ const User = require('../mongodb/User')
 
 function authenticate (username, hash) {
   return User.findOne({ username }, (err, user) => {
-    if (err) return process.stdout.write(err)
+    if (err) return process.stdout.write(err.message)
+    if (!user) return null
     return user.hash === hash ? user : null
   })
 }
--- graphql/root.js
+++ graphql/root.js
@@ -10,11 +10,12 @@ const resolvers = {
 
   // User mutations
   createUser: ({ username, hash }) => {
-    if (User.find({ username })) return null
-    return new User({
+    const user = new User({
       username,
       hash
     }).save()
+
+    return user
   },
   deleteUser: ({ id }) => {
     return User.findOneAndDelete({ _id: id })
@@ -22,8 +23,13 @@ const resolvers = {
 
   // snippet mutations
   createSnippet: async (data) => {
+    console.log(data)
     const user = await authenticate(data.username, data.hash)
-    if (!user) { throw new Error(`No user exists for the specified username: ${data.username}`) }
+    if (!user) {
+      return new Error(
+        `No user exists for the specified username: ${data.username}`
+      )
+    }
 
     const snippet = await new Snippet({
       user,
@@ -38,25 +44,55 @@ const resolvers = {
 
     return snippet
   },
-  updateSnippet: async (data) => {
+  updateSnippet: async data => {
     const user = await authenticate(data.username, data.hash)
-    if (!user) { throw new Error(`No user exists for the specified username: ${data.username}`) }
-
-    Snippet.findByIdAndUpdate({})
+    if (!user) {
+      throw new Error(
+        `No user exists for the specified username: ${data.username}\n`
+      )
+    }
+    const ID = user.snippets.map(snip => {
+      return data.original.title === snip.title
+    })[0]._id
+    Snippet.findByIdAndUpdate(ID, data.updated).save()
   },
-  deleteSnippet: ({ id }) => {
-    return Snippet.findOneAndDelete({ _id: id })
+  deleteSnippet: async data => {
+    const user = await authenticate(data.username, data.hash)
+      .populate({ path: 'snippets', model: 'Snippet' })
+      .exec()
+    if (!user) {
+      throw new Error(
+        `No user exists for the specified username: ${data.username}`
+      )
+    }
+    const snippetToDelete = user.snippets.map(snippet => {
+      return (data.title === snippet.title) && snippet
+    })
+
+    user.snippets = user.snippets.map(snippet => {
+      return (data.title !== snippet.title) && snippet
+    })
+    console.log(user)
+    user.save()
+
+    console.log(user)
+
+    return Snippet.findOneAndDelete(snippetToDelete)
   },
 
   // user queries
   getUser: async ({ username, hash }) => {
-    return authenticate(username, hash).populate({ path: 'snippets', model: 'Snippet' }).exec()
+    return authenticate(username, hash)
+      .populate({ path: 'snippets', model: 'Snippet' })
+      .exec()
   },
 
   // snippet queries
   getSnippet: async ({ username, hash, title }) => {
     const user = await authenticate(username, hash)
-    if (!user) { throw new Error(`No user exists for the specified username: ${username}`) }
+    if (!user) {
+      throw new Error(`No user exists for the specified username: ${username}`)
+    }
     const snippet = await Snippet.findOne({
       user,
       title
